350ddb543f1e742aea0d55a9199515c31b78462e
Alexandre Bulté <alexandre@bulte.net>
2020-12-23 20:19:16 +0100

    Separate catalog and crawl, make way for curses delight

--- cli.py
+++ cli.py
@@ -1,27 +1,14 @@
-import os
 import csv
-import json
-import time
+import os
+from tempfile import NamedTemporaryFile
 
 import aiohttp
-import asyncio
 import asyncpg
-
-from datetime import datetime, timedelta
-from tempfile import NamedTemporaryFile
-from urllib.parse import urlparse
-
-from humanfriendly import parse_timespan
 from minicli import cli, run, wrap
 from progressist import ProgressBar
-from tabulate import tabulate
 
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/postgres')
 CATALOG_URL = 'https://www.data.gouv.fr/fr/datasets/r/4babf5f2-6a9c-45b5-9144-ca5eae6a7a6d'
-
-# max number of _completed_ requests per domain per period
-BACKOFF_NB_REQ = 1800
-BACKOFF_PERIOD = 3600  # in seconds
 
 context = {}
 
@@ -29,13 +16,12 @@ context = {}
 @cli
 async def init_db(drop=False, table=None):
     """Create the DB structure"""
-    conn = await asyncpg.connect(dsn=DATABASE_URL)
     if drop:
         if table == 'catalog' or not table:
-            await conn.execute('DROP TABLE catalog')
+            await context['conn'].execute('DROP TABLE catalog')
         if table == 'checks' or not table:
-            await conn.execute('DROP TABLE checks')
-    await conn.execute('''
+            await context['conn'].execute('DROP TABLE checks')
+    await context['conn'].execute('''
         CREATE TABLE IF NOT EXISTS catalog(
             id serial PRIMARY KEY,
             dataset_id VARCHAR(24),
@@ -45,7 +31,7 @@ async def init_db(drop=False, table=None):
             UNIQUE(dataset_id, resource_id, url)
         )
     ''')
-    await conn.execute('''
+    await context['conn'].execute('''
         CREATE TABLE IF NOT EXISTS checks(
             id serial PRIMARY KEY,
             url VARCHAR,
@@ -57,13 +43,11 @@ async def init_db(drop=False, table=None):
             response_time FLOAT
         )
     ''')
-    await conn.close()
 
 
 @cli
 async def load_catalog(url=CATALOG_URL):
     """Load the catalog into DB from CSV file"""
-    conn = await asyncpg.connect(dsn=DATABASE_URL)
     try:
         print('Downloading catalog...')
         fd = NamedTemporaryFile(delete=False)
@@ -83,7 +67,7 @@ async def load_catalog(url=CATALOG_URL):
             rows = list(reader)
             bar = ProgressBar(total=len(rows))
             for row in bar.iter(rows):
-                await context['conn'].execute(f'''
+                await context['conn'].execute('''
                     INSERT INTO catalog (dataset_id, resource_id, url, deleted)
                     VALUES ($1, $2, $3, FALSE)
                     ON CONFLICT (dataset_id, resource_id, url) DO UPDATE SET deleted = FALSE
@@ -94,148 +78,13 @@ async def load_catalog(url=CATALOG_URL):
     finally:
         fd.close()
         os.unlink(fd.name)
-        await conn.close()
 
-
-async def insert_check(data):
-    columns = ','.join(data.keys())
-    # $1, $2...
-    placeholders = ','.join([f'${x + 1}' for x in range(len(data.values()))])
-    q = f'''
-        INSERT INTO checks ({columns})
-        VALUES ({placeholders})
-    '''
-    async with context['pool'].acquire() as connection:
-        await connection.execute(q, *data.values())
-
-
-async def is_backoff(domain):
-    since = datetime.utcnow() - timedelta(seconds=BACKOFF_PERIOD)
-    async with context['pool'].acquire() as connection:
-        res = await connection.fetch('''
-            SELECT COUNT(*) FROM checks
-            WHERE domain = $1
-            AND created_at >= $2
-        ''', domain, since)
-        print(res[0]['count'])
-        return res[0]['count'] >= BACKOFF_NB_REQ
-
-
-async def check_url(row, session, sleep=0):
-
-    # TODO: make this a (data)class?
-    def return_structure(status, url, details=None):
-        return {
-            'status': status,
-            'url': url,
-            'details': details,
-        }
-
-    if sleep:
-        await asyncio.sleep(sleep)
-
-    url_parsed = urlparse(row['url'])
-    domain = url_parsed.netloc
-    if not domain:
-        print(f"[warning] not netloc in url, skipping {row['url']}")
-        return
-
-    if await is_backoff(domain):
-        print(f'backoff {domain}')
-        return await check_url(row, session, timeout, sleep=BACKOFF_PERIOD / BACKOFF_NB_REQ)
-    else:
-        print('not backing off')
-
-    try:
-        start = time.time()
-        timeout = aiohttp.ClientTimeout(total=5)
-        async with session.head(row['url'], timeout=timeout, allow_redirects=True) as resp:
-            end = time.time()
-            # /!\ this will only take the first value for a given header key
-            # but multidict is not json serializable
-            headers = {}
-            for k in resp.headers.keys():
-                # FIX Unicode low surrogate must follow a high surrogate.
-                # eg in 'TREMI_2017-R\xe9sultats enqu\xeate bruts.csv'
-                value = resp.headers[k].encode('utf-8', 'surrogateescape').decode('utf-8', 'replace')
-                headers[k.lower()] = value
-            await insert_check({
-                'url': row['url'],
-                'domain': domain,
-                'status': resp.status,
-                'headers': json.dumps(headers),
-                'timeout': False,
-                'response_time': end - start,
-            })
-            return return_structure('ok', row['url'], None)
-    except aiohttp.client_exceptions.ClientError as e:
-        # TODO: store results
-        return return_structure('error', row['url'], e)
-    except asyncio.exceptions.TimeoutError:
-        await insert_check({
-            'url': row['url'],
-            'timeout': True,
-        })
-        return return_structure('timeout', row['url'], None)
-
-
-# TODO:
-# - backoff on domain (no more than XX calls on the same domain for YY time)
-# - domain exclude list
-@cli
-async def crawl(since='4w', domain=None):
-    """Crawl the catalog
-
-    :since: check urls not checked since timespan, defaults to 4w(eeks)
-    """
-    since = parse_timespan(since)  # in seconds
-    since = datetime.now() - timedelta(seconds=since)
-    async with context['pool'].acquire() as connection:
-        res = await connection.fetch('''
-            SELECT url FROM catalog
-            WHERE url LIKE $1
-            -- this is way too slow... index?
-            -- AND url NOT IN (SELECT url FROM checks WHERE created_at >= $2)
-            ORDER BY random()
-            LIMIT 10
-        ''',
-        'https://static.data.gouv.fr%',
-        # since,
-        )
-    tasks = []
-    results = []
-    async with aiohttp.ClientSession(timeout=None) as session:
-        for row in res:
-            tasks.append(check_url(row, session))
-        bar = ProgressBar(total=len(res))
-        for task in asyncio.as_completed(tasks):
-            result = await task
-            results.append(result)
-            bar.update()
-    # TODO: print summary
-    # - total checked, aggregated by domain?
-    # - total left to check from catalog (because limit)
-    oks = [r for r in results if r['status'] == 'ok']
-    timeouts = [r for r in results if r['status'] == 'timeout']
-    errors = [r for r in results if r['status'] == 'error']
-    table = [
-        ['checked urls', len(tasks)],
-        ['oks', len(oks)],
-        ['timeouts', len(timeouts)],
-        ['errors', len(errors)],
-    ]
-    print(f'\n{tabulate(table)}')
-    if errors:
-        print('\nErrors:')
-        for r in errors:
-            print('-' * 7)
-            print(r['url'])
-            print(r['details'])
 
 @wrap
-async def wrapper():
-    context['pool'] = await asyncpg.create_pool(dsn=DATABASE_URL)
+async def cli_wrapper():
+    context['conn'] = await asyncpg.connect(dsn=DATABASE_URL)
     yield
+    await context['conn'].close()
 
 
 if __name__ == '__main__':
---
+++ crawl.py
@@ -0,0 +1,205 @@
+import curses
+import json
+import os
+import time
+
+from datetime import datetime, timedelta
+from urllib.parse import urlparse
+
+import aiohttp
+import asyncio
+import asyncpg
+
+from humanfriendly import parse_timespan
+from tabulate import tabulate
+
+context = {}
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/postgres')
+# max number of _completed_ requests per domain per period
+BACKOFF_NB_REQ = 3600 * 2
+BACKOFF_PERIOD = 3600  # in seconds
+
+
+async def insert_check(data):
+    columns = ','.join(data.keys())
+    # $1, $2...
+    placeholders = ','.join([f'${x + 1}' for x in range(len(data.values()))])
+    q = f'''
+        INSERT INTO checks ({columns})
+        VALUES ({placeholders})
+    '''
+    async with context['pool'].acquire() as connection:
+        await connection.execute(q, *data.values())
+
+
+async def is_backoff(domain):
+    since = datetime.utcnow() - timedelta(seconds=BACKOFF_PERIOD)
+    async with context['pool'].acquire() as connection:
+        res = await connection.fetch('''
+            SELECT COUNT(*) FROM checks
+            WHERE domain = $1
+            AND created_at >= $2
+        ''', domain, since)
+        return res[0]['count'] >= BACKOFF_NB_REQ
+
+
+async def check_url(row, session, sleep=0):
+
+    # TODO: make this a (data)class?
+    def return_structure(status, url, details=None):
+        return {
+            'status': status,
+            'url': url,
+            'details': details,
+        }
+
+    if sleep:
+        await asyncio.sleep(sleep)
+
+    url_parsed = urlparse(row['url'])
+    domain = url_parsed.netloc
+    if not domain:
+        print(f"[warning] not netloc in url, skipping {row['url']}")
+        return
+
+    if await is_backoff(domain):
+        # TODO: move to curses, it does _not_ like printed stuff while in curses window
+        print(f'backoff {domain}')
+        return await check_url(row, session, sleep=BACKOFF_PERIOD / BACKOFF_NB_REQ)
+
+    try:
+        start = time.time()
+        timeout = aiohttp.ClientTimeout(total=5)
+        async with session.head(row['url'], timeout=timeout, allow_redirects=True) as resp:
+            end = time.time()
+            # /!\ this will only take the first value for a given header key
+            # but multidict is not json serializable
+            headers = {}
+            for k in resp.headers.keys():
+                # FIX Unicode low surrogate must follow a high surrogate.
+                # eg in 'TREMI_2017-R\xe9sultats enqu\xeate bruts.csv'
+                value = resp.headers[k]\
+                        .encode('utf-8', 'surrogateescape')\
+                        .decode('utf-8', 'replace')
+                headers[k.lower()] = value
+            await insert_check({
+                'url': row['url'],
+                'domain': domain,
+                'status': resp.status,
+                'headers': json.dumps(headers),
+                'timeout': False,
+                'response_time': end - start,
+            })
+            return return_structure('ok', row['url'], None)
+    except aiohttp.client_exceptions.ClientError as e:
+        # TODO: store results
+        return return_structure('error', row['url'], e)
+    except asyncio.exceptions.TimeoutError:
+        await insert_check({
+            'url': row['url'],
+            'timeout': True,
+        })
+        return return_structure('timeout', row['url'], None)
+
+
+# TODO: track more stuff on screen
+def refresh_summary(results):
+    screen = context['screen']
+    screen.addstr(0, 0, f"{len(results)}\n", curses.A_BOLD)
+    screen.refresh()
+
+
+def print_summary(results):
+    # TODO: print summary
+    # - total checked, aggregated by domain?
+    # - total left to check from catalog (because limit)
+    # - print on errors too (finally:)
+    oks = [r for r in results if r['status'] == 'ok']
+    timeouts = [r for r in results if r['status'] == 'timeout']
+    errors = [r for r in results if r['status'] == 'error']
+    table = [
+        ['checked urls', len(results)],
+        ['oks', len(oks)],
+        ['timeouts', len(timeouts)],
+        ['errors', len(errors)],
+    ]
+    print(f'\n{tabulate(table)}')
+    if errors:
+        print('\nErrors:')
+        for r in errors:
+            print('-' * 7)
+            print(r['url'])
+            print(r['details'])
+
+
+async def crawl_urls(to_parse, results):
+    tasks = []
+    async with aiohttp.ClientSession(timeout=None) as session:
+        for row in to_parse:
+            tasks.append(check_url(row, session))
+        for task in asyncio.as_completed(tasks):
+            result = await task
+            results.append(result)
+            refresh_summary(results)
+
+
+# TODO:
+# - domain exclude list
+async def crawl(since='4w', domain=None):
+    """Crawl the catalog
+
+    :since: check urls not checked since timespan, defaults to 4w(eeks)
+    """
+    context['pool'] = await asyncpg.create_pool(dsn=DATABASE_URL)
+
+    since = parse_timespan(since)  # in seconds
+    since = datetime.now() - timedelta(seconds=since)
+    async with context['pool'].acquire() as connection:
+        to_parse = await connection.fetch(
+            '''
+                SELECT url FROM catalog
+                WHERE url LIKE $1
+                -- this is way too slow... index?
+                -- AND url NOT IN (SELECT url FROM checks WHERE created_at >= $2)
+                ORDER BY random()
+                LIMIT 100
+            ''',
+            'https://static.data.gouv.fr%',
+            # since,
+        )
+    results = []
+    await crawl_urls(to_parse, results)
+    return results
+
+
+def run():
+    try:
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(1)
+        try:
+            curses.start_color()
+        except:  # noqa
+            pass
+        context['screen'] = stdscr
+        # TODO: this will get ugly in memory, only aggregated stuff in results
+        # maybe make it a "global" var, might display if interrupted
+        # use a log file to output complete run (csv?)
+        results = asyncio.get_event_loop().run_until_complete(crawl())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Set everything back to normal
+        if 'stdscr' in locals():
+            stdscr.keypad(0)
+            curses.echo()
+            curses.nocbreak()
+            curses.endwin()
+        if results:
+            print_summary(results)
+
+
+if __name__ == "__main__":
+    run()
