b39ba823977d5c3ef2c53f62cb82c141d5849073
Alexandre Bulté <alexandre@bulte.net>
2021-02-17 10:13:39 +0100

    fix cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ nest_asyncio.apply()
 
 
 async def test_report(setup_catalog, fake_check, tmp_path):
-    await fake_check()
+    await fake_check(headers={"last-modified": "Wed, 21 Oct 2015 07:28:00 GMT"})
     report = tmp_path / "test.html"
     run("report", filepath=report.__str__())
     assert report.exists()
